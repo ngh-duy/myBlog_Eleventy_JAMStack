145b021e9c1eab58a25046dbdf2969d955c053d9
21DTHA2_2180605768 <duynguyenhoang203@gmail.com>
2025-04-10 09:28:16 +0700

    Upload CMS

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
   // Copy thư mục public/css
   eleventyConfig.addPassthroughCopy("public");
+  eleventyConfig.addPassthroughCopy("admin");
+
   // Filter định dạng ngày
   eleventyConfig.addFilter("date", function(value, format = "MMM dd, yyyy") {
     return DateTime.fromJSDate(value, { zone: 'utc' }).toFormat(format);
